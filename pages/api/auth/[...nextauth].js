ce9e64720609ecb1bb0fedef62108089985ae2fe
danpoj <jws970306@khu.ac.kr>
2022-12-29 21:50:50 +0900

    chore next-auth port 3005

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -16,15 +16,13 @@ export const authOptions = {
       // account의 값이 평소에는 undefined,
       // 🔥로그인 할 때만 account, profile 값이 유저 정보로 채워짐
       // 그래서 if(account) 조건문으로, 로그인 될 때만 서버에 유저 정보 보내주는게 아래 코드
-      // http://localhost:8080/users 는 제가 짠 임시 end-point, end point는 어디든 상관없어요
       if (account) {
-        await axios.post('http://localhost:8080/users', {
+        await axios.post('http://localhost:3005/users', {
           name: profile.name,
           email: profile.email,
           image: profile.picture,
         })
       }
-      console.log(profile)
 
       return token
     },
