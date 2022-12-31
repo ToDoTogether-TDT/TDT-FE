6917678161d95f3d713d712972f6a3f3c9899912
danpoj <jws970306@khu.ac.kr>
2022-12-31 13:45:21 +0900

    feat: study page navigation

--- components/AddToDoForm.js
+++ components/AddToDoForm.js
@@ -1,10 +1,15 @@
+import axios from 'axios'
 import { useState } from 'react'
+import { mutate } from 'swr'
 import { v4 as uuidv4 } from 'uuid'
+import useDate from '../lib/getCurrentDate'
 
-const AddToDoForm = () => {
+const AddToDoForm = ({ study, setIsToDo }) => {
   const [toDos, setToDos] = useState([])
   const [toDo, setToDo] = useState('')
   const [isFormError, setIsFormError] = useState(false)
+  const [isDelete, setIsDelete] = useState(false)
+  const [year, month, day, onYearChange, onMonthChange, onDayChange] = useDate()
 
   const onAddToDo = (e) => {
     e.preventDefault()
@@ -23,43 +28,105 @@ const AddToDoForm = () => {
     }
   }
 
+  const onDeleteButtonClick = () => {
+    setIsDelete((prev) => !prev)
+  }
+
+  const deleteToDo = (id) => {
+    const newToDos = toDos.filter((toDo) => toDo.id !== id)
+    setToDos(newToDos)
+  }
+
+  const onPostingButtonClcik = async () => {
+    const res = await axios.patch(
+      `http://localhost:3005/studies/${study[0].id}`,
+      {
+        todos: [
+          {
+            date: `${year}/${month}/${day}`,
+            lists: toDos,
+          },
+          ...study[0].todos,
+        ],
+      }
+    )
+
+    if (res.statusText === 'OK') {
+      mutate(`${process.env.NEXT_PUBLIC_BASE_URL}/studies?id=${study[0].id}`)
+      setIsToDo(false)
+    }
+  }
+
   return (
     <div className='px-4 py-8'>
+      {/* title + delete button */}
       <div className='flex items-center gap-4 mb-8'>
-        <h1 className='text-3xl font-black'>ToDos</h1>
-        {/* <button
-          onClick={onAddToDoButtonClick}
-          className='border-2 border-slate-400 text-slate-400 text-xl font-bold w-10 h-10 rounded-lg hover:border-slate-800 hover:text-slate-800 transition'
+        <h1 className='text-3xl font-extralight'>TODOs</h1>
+        <button
+          onClick={onDeleteButtonClick}
+          className='text-sm border text-red-500 border-red-500 py-3 px-5 rounded'
         >
-          +
-        </button> */}
+          {isDelete ? '완료 🚀' : '삭제 👋'}
+        </button>
       </div>
+
+      {/* date */}
+      <div className='mb-8 flex gap-2 items-end'>
+        <input
+          className='border-b border-black h-18 w-20 font-mono text-xl text-center outline-none'
+          type='number'
+          value={year}
+          onChange={onYearChange}
+        />
+        <div className='font-thin text-xl'>년</div>
+        <input
+          className='border-b border-black h-18 w-20 font-mono text-xl text-center outline-none'
+          type='number'
+          value={month}
+          onChange={onMonthChange}
+        />
+        <div className='font-thin text-xl'>월</div>
+        <input
+          className='border-b border-black h-18 w-20 font-mono text-xl text-center outline-none'
+          type='number'
+          value={day}
+          onChange={onDayChange}
+        />
+        <div className='font-thin text-xl'>일</div>
+      </div>
+
       <div className='flex flex-col gap-4'>
         {toDos.map((item, i) => (
-          <>
-            <div
-              key={item.id}
-              className='flex items-center gap-4 border border-stone-300 p-3   relative rounded'
-            >
-              <span className='font-bold text-xs text-white bg-indigo-500 rounded-full w-[18px] h-[18px] flex items-center justify-center absolute -left-2 -top-2'>
-                {i + 1}
-              </span>
-              <button className='text-rose-400 font-black text-xl absolute -right-2 -top-3 bg-white rounded-full'>
-                ⓧ
+          // todos
+          <div
+            key={item.id + i}
+            className='flex items-center gap-4 p-3 relative rounded shadow bg-white'
+          >
+            <span className='font-bold text-xs text-white bg-indigo-500 rounded-full w-[18px] h-[18px] flex items-center justify-center'>
+              {i + 1}
+            </span>
+            {isDelete && (
+              <button
+                onClick={() => deleteToDo(item.id)}
+                className=' absolute -left-[8px] -top-2 bg-white'
+              >
+                👋
               </button>
+            )}
 
-              <p className='text-sm'>{item.text}</p>
-            </div>
-          </>
+            <p className='text-sm'>{item.text}</p>
+          </div>
         ))}
-        <form onSubmit={onAddToDo} className='flex gap-4 items-center'>
+
+        {/* add todo form */}
+        <form onSubmit={onAddToDo} className='flex gap-2 items-center'>
           <input
-            className='border w-full rounded-lg outline-none p-4'
+            className='border w-full h-12 rounded outline-none p-4'
             type='text'
             value={toDo}
             onChange={onInputFormChange}
           />
-          <button className='border text-sm border-black w-24 py-3 px-4 rounded'>
+          <button className='border text-sm border-black w-24 py-3 px-3 rounded'>
             추가 🎉
           </button>
         </form>
@@ -67,6 +134,12 @@ const AddToDoForm = () => {
           <p className='text-rose-600 text-sm'>내용을 입력해주세요 😅</p>
         )}
       </div>
+      <button
+        onClick={onPostingButtonClcik}
+        className='border text-sm border-black w-24 py-3 px-3 rounded mt-6'
+      >
+        게시 🔖
+      </button>
     </div>
   )
 }
--- pages/programming/[id].js
+++ pages/programming/[id].js
@@ -4,10 +4,17 @@ import AddToDoForm from '../../components/AddToDoForm'
 import Layout from '../../components/Layout'
 import { useFetch } from '../../lib/useFetch'
 
+const pageType = {
+  TODOS: 'TODOS',
+  INTRO: 'INTRO',
+  MEMBER: 'MEMBER',
+}
+
 export default function StudyPost() {
   const router = useRouter()
   const study = useFetch('studies', 'id', router.query.id)
   const [isToDo, setIsToDo] = useState(false)
+  const [page, setPage] = useState(pageType.TODOS)
 
   const onToDoBtnClick = () => {
     setIsToDo((prevState) => !prevState)
@@ -18,6 +25,7 @@ export default function StudyPost() {
   return (
     <Layout>
       <div className='px-4'>
+        {/* 글쓴이 정보, todo 추가 버튼 */}
         <div className='flex justify-between'>
           <div className='flex items-center gap-2'>
             <img
@@ -35,19 +43,75 @@ export default function StudyPost() {
           </button>
         </div>
         {isToDo ? (
-          <AddToDoForm />
+          <AddToDoForm study={study} setIsToDo={setIsToDo} />
         ) : (
           <>
-            <div className='py-6 border-b'>
-              <div className='text-4xl font-black mb-4'>{study[0].title}</div>
-              <div className='text-xl'>{study[0].introduction}</div>
+            {/* 스터디 제목, 짧은 소개 */}
+            <div className='py-8 mb-8'>
+              <div className='text-4xl font-black mb-4 text-indigo-600'>
+                {study[0].title}
+              </div>
+              <div className='text-slate-500'>{study[0].introduction}</div>
             </div>
-            <div className='flex flex-col gap-2 mt-7'>
+
+            {/* 페이지 전환 버튼 */}
+            <div className='text-lg flex gap-7 border-b border-slate-600 py-4'>
+              <button
+                onClick={() => setPage(pageType.TODOS)}
+                className={
+                  page === pageType.TODOS ? 'underline' : 'text-slate-400'
+                }
+              >
+                TODOs
+              </button>
+              <button
+                onClick={() => setPage(pageType.INTRO)}
+                className={
+                  page === pageType.INTRO ? 'underline' : 'text-slate-400'
+                }
+              >
+                스터디 소개
+              </button>
+              <button
+                onClick={() => setPage(pageType.MEMBER)}
+                className={
+                  page === pageType.MEMBER ? 'underline' : 'text-slate-400'
+                }
+              >
+                스터디 구성원
+              </button>
+            </div>
+
+            {/* TODOs */}
+            {page === pageType.TODOS && (
+              <div className='flex flex-col gap-32 mt-10 '>
+                {study[0].todos.map((todo) => (
+                  <div className='' key={todo.date}>
+                    <p className='text-xl font-light mb-4'>
+                      {todo.date.replace(/\//g, '. ')}
+                    </p>
+                    <ul className='text-sm flex flex-col gap-2'>
+                      {todo.lists.map((list, i) => (
+                        <li className='p-2 flex gap-3' key={list.id}>
+                          <span className='font-bold text-xs text-white bg-indigo-500 rounded-full w-[16px] h-[16px] flex items-center justify-center'>
+                            {i + 1}
+                          </span>
+                          <span>{list.text}</span>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                ))}
+              </div>
+            )}
+
+            {/* 스터디 소개 */}
+            {page === pageType.INTRO && (
               <div
-                className='markdown-body'
+                className='markdown-body flex flex-col gap-2 mt-7'
                 dangerouslySetInnerHTML={{ __html: study[0].content }}
               ></div>
-            </div>
+            )}
           </>
         )}
       </div>
